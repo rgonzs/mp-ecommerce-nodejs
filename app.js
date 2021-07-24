c9bfd3cd9a3d4216c25493d531d027980e21f147
Renato Gonzales <rent.gonz@gmail.com>
2021-07-24 12:18:28 -0500

    Finalizado proyecto

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.post('/webhook', (req, res)=>{
     console.log('----------QUERY----------')
     console.log(req.query)
     console.log('----------BODY----------')
-    console.log(req)
+    console.log(req.body)
     res.status(200).json({})
 })
 
